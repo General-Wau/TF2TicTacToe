f59de3b841d69a7d2673337582b36453a33f8754
General-Wau <elven@gmx.com>
2021-04-12 17:57:39 +0200

    The first update of tf2 theme for the game . + New character select activity is added as the launcher activity + Updated the appearance of the game screen + Added all the sound and image resources
    Upcoming features :
    - Implement customized icons for each character
    - Implement sounds for each character
    - Add logic for handling characters
    - Update game activity to handle new features
    - Add optional new game or next round buttons
    - Add thread wait after selecting 2nd character

---
+++ app/src/main/java/com/necromyd/tictactoe/CharSelect.java
@@ -0,0 +1,14 @@
+package com.necromyd.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CharSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_char_select);
+    }
+}
